43cf777ed05a71969999430b423a2267f6b1c9ca
Dimpal-lad <53107592+Dimpal-lad@users.noreply.github.com>
2025-03-07 22:48:18 -0500

    Profile page added

--- GroceryGenie/backend/src/main.py
+++ GroceryGenie/backend/src/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="fronend/templates")
 app.secret_key = 'supersecretkey'  # Change this for security
 
 # Mock database (Replace this with an actual database)
