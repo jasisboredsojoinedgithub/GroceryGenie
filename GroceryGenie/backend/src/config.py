1922a97fcf446d6dd30edaf85644a02f7b62abc9
Dimpal-lad <53107592+Dimpal-lad@users.noreply.github.com>
2025-03-24 21:08:19 -0400

    Updated .gitignore

--- GroceryGenie/backend/src/config.py
+++ GroceryGenie/backend/src/config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 # Load environment variables
-env_path = os.path.join(os.path.dirname(__file__), "env.env")
+env_path = os.path.join(os.path.dirname(__file__), ".env")
 load_dotenv(dotenv_path=env_path)
 
 SECRET_KEY = os.getenv("SECRET_KEY")
