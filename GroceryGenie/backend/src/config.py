654355a941482802626b30688a1cbd0c2119b7b8
Rishika Shankara <146372452+Rishikaashankar@users.noreply.github.com>
2025-04-21 12:17:20 -0400

    Update config.py

--- GroceryGenie/backend/src/config.py
+++ GroceryGenie/backend/src/config.py
@@ -1,13 +1,15 @@
 import os
-from dotenv import load_dotenv
-from pathlib import Path
 
-# Load environment variables
-env_path = Path(__file__).resolve().parent.parent.parent.parent / ".env"
-# env_path = os.path.join(os.path.dirname(__file__), ".env")
-load_dotenv(dotenv_path=env_path)
+# ONLY load .env locally — skip it in CI
+if os.getenv("GITHUB_ACTIONS") != "true":
+    from dotenv import load_dotenv
+    from pathlib import Path
+    env_path = Path(__file__).resolve().parent.parent.parent.parent / ".env"
+    load_dotenv(dotenv_path=env_path)
 
+# Use environment variables
 SECRET_KEY = os.getenv("SECRET_KEY")
 MONGO_URI = os.getenv("MONGO_URI")
 AZURE_STORAGE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 AZURE_STORAGE_CONTAINER_NAME = os.getenv("AZURE_STORAGE_CONTAINER_NAME")
+AZURE_STORAGE_CONTAINER_GROCERY = os.getenv("AZURE_STORAGE_CONTAINER_GROCERY")
